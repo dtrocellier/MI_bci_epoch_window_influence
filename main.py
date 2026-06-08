f923a5b905d2b44f2a0397bf6dc7eac47c430ca7
Simon Kojima <simon.kojima@inria.fr>
2026-06-08 12:39:13 +0200

    update

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def run(cfg):
         print(f"device:{device}, cuDNN enabled: {torch.backends.cudnn.enabled}")
 
     with wandb.init(
-            project=cfg.wandb.project,
-            mode=cfg.wandb.mode,
-            name=run_name,
-            config=OmegaConf.to_container(cfg, resolve=True),
+        project=cfg.wandb.project,
+        mode=cfg.wandb.mode,
+        name=run_name,
+        config=OmegaConf.to_container(cfg, resolve=True),
     ):
 
         func_build_dataset = get_func_build_dataset(cfg.model.func_build_dataset)
@@ -65,9 +65,9 @@ def run(cfg):
             best_acc = 0
             save_name = f"{cfg.model.batch_size}_epochs_{cfg.model[pipeline_name].n_epochs}_batch_size_{cfg.model[pipeline_name].optimizer.kwargs.lr}_lr"
             best_model_path = (
-                    Path("model")
-                    / cfg.model.name
-                    / f"{cfg.model.name}_{cfg.dataset.name}_{cfg.epoch_window.name}_{cfg.subject}_{save_name}.pt"
+                Path("model")
+                / cfg.model.name
+                / f"{cfg.model.name}_{cfg.dataset.name}_{cfg.epoch_window.name}_{cfg.subject}_{save_name}.pt"
             )
             best_model_path.parent.mkdir(exist_ok=True, parents=True)
 
@@ -98,7 +98,8 @@ def run(cfg):
                     torch.save(model.state_dict(), best_model_path)
 
                 print(
-                    f"{epoch + 1:03d}, train_loss: {train_loss:.4f}, train_acc: {train_acc:.4f}, valid_loss: {valid_loss:.4f}, valid_acc: {valid_acc:.4f}, duration: {duration:.2f}s, lr: {scheduler.get_last_lr()[0]:.4f}")
+                    f"{epoch + 1:03d}, train_loss: {train_loss:.4f}, train_acc: {train_acc:.4f}, valid_loss: {valid_loss:.4f}, valid_acc: {valid_acc:.4f}, duration: {duration:.2f}s, lr: {scheduler.get_last_lr()[0]:.4f}"
+                )
                 wandb.log(
                     {
                         "train_loss": train_loss,
@@ -123,6 +124,8 @@ def run(cfg):
 
 @hydra.main(version_base=None, config_path="conf_debug", config_name="config")
 def main(cfg):
+    print(cfg)
+    exit()
     set_seed(cfg.seed)
     run(cfg)
 
